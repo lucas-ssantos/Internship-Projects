f35f1dac4328a53f2c3010e28dde3b536a7583a7
Lucas Santos <108681755+lucas-ssantos@users.noreply.github.com>
2023-07-15 18:24:21 -0300

    Update salaobelezax.sql
    Forgot to change the INSERT row that was senha_cliente

--- SalaoBelezaX/aplicacao/salaobelezax.sql
+++ SalaoBelezaX/aplicacao/salaobelezax.sql
@@ -12,4 +12,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id_user`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
-INSERT INTO `users` (`id_user`, `nome_user`, `email_user`, `senha_cliente`, `adm_user`) VALUES (NULL, 'admin', 'admin@gmail.com', MD5('admin'), '1');
+INSERT INTO `users` (`id_user`, `nome_user`, `email_user`, `senha_user`, `adm_user`) VALUES (NULL, 'admin', 'admin@gmail.com', MD5('admin'), '1');
